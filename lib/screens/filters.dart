130292f77aee877d86bcf40340507f98bd44cc7d
Papa97 <65426684+Papa97@users.noreply.github.com>
2025-04-16 13:40:36 +0200

    first commit

---
+++ lib/screens/filters.dart
@@ -0,0 +1,178 @@
+import 'package:flutter/material.dart';
+// import 'package:meals/screens/tabs.dart';
+// import 'package:meals/widgets/main_drawer.dart';
+
+enum Filter {
+  glutenFree,
+  lactoseFree,
+  vegetarian,
+  vegan,
+}
+
+class FiltersScreen extends StatefulWidget {
+  const FiltersScreen({super.key, required this.currentFilters});
+
+  final Map<Filter, bool> currentFilters;
+
+  @override
+  State<FiltersScreen> createState() => _FiltersScreenState();
+}
+
+class _FiltersScreenState extends State<FiltersScreen> {
+  bool _glutenFreeFilterSet = false;
+  bool _lactoseFreeFilterSet = false;
+  bool _vegetarianFilterSet = false;
+  bool _veganFilterSet = false;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+
+    _glutenFreeFilterSet = widget.currentFilters[Filter.glutenFree]!;
+    _lactoseFreeFilterSet = widget.currentFilters[Filter.lactoseFree]!;
+    _vegetarianFilterSet = widget.currentFilters[Filter.vegetarian]!;
+    _veganFilterSet = widget.currentFilters[Filter.vegan]!;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Filters'),
+      ),
+      // drawer: MainDrawer(onSelectScreen: (identifier) {
+      //   Navigator.of(context).pop();
+
+      //   if (identifier == 'meals') {
+      //     Navigator.of(context).pushReplacement(
+      //         MaterialPageRoute(builder: (ctx) => const TabsScreen()));
+      //   }
+      // }),
+      body: PopScope(
+        canPop: false,
+        onPopInvokedWithResult: (bool didPop, dynamic result) {
+          if (didPop) return;
+          Navigator.of(context).pop({
+            Filter.glutenFree: _glutenFreeFilterSet,
+            Filter.lactoseFree: _lactoseFreeFilterSet,
+            Filter.vegetarian: _vegetarianFilterSet,
+            Filter.vegan: _veganFilterSet,
+          });
+        },
+        child: Column(
+          children: [
+            SwitchListTile(
+              contentPadding: const EdgeInsets.only(
+                left: 34,
+                right: 22,
+              ),
+              activeColor: Theme.of(context).colorScheme.tertiary,
+              subtitle: Text(
+                'Only include gluten-free meals.',
+                style: Theme.of(context)
+                    .textTheme
+                    .labelMedium!
+                    .copyWith(color: Theme.of(context).colorScheme.onSurface),
+              ),
+              title: Text(
+                'Gluten free',
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge!
+                    .copyWith(color: Theme.of(context).colorScheme.onSurface),
+              ),
+              value: _glutenFreeFilterSet,
+              onChanged: (isChecked) {
+                setState(() {
+                  _glutenFreeFilterSet = isChecked;
+                });
+              },
+            ),
+            SwitchListTile(
+              contentPadding: const EdgeInsets.only(
+                left: 34,
+                right: 22,
+              ),
+              activeColor: Theme.of(context).colorScheme.tertiary,
+              subtitle: Text(
+                'Only include lactose-free meals.',
+                style: Theme.of(context)
+                    .textTheme
+                    .labelMedium!
+                    .copyWith(color: Theme.of(context).colorScheme.onSurface),
+              ),
+              title: Text(
+                'Lactose free',
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge!
+                    .copyWith(color: Theme.of(context).colorScheme.onSurface),
+              ),
+              value: _lactoseFreeFilterSet,
+              onChanged: (isChecked) {
+                setState(() {
+                  _lactoseFreeFilterSet = isChecked;
+                });
+              },
+            ),
+            SwitchListTile(
+              contentPadding: const EdgeInsets.only(
+                left: 34,
+                right: 22,
+              ),
+              activeColor: Theme.of(context).colorScheme.tertiary,
+              subtitle: Text(
+                'Only include vegetarian meals.',
+                style: Theme.of(context)
+                    .textTheme
+                    .labelMedium!
+                    .copyWith(color: Theme.of(context).colorScheme.onSurface),
+              ),
+              title: Text(
+                'Vegetarian',
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge!
+                    .copyWith(color: Theme.of(context).colorScheme.onSurface),
+              ),
+              value: _vegetarianFilterSet,
+              onChanged: (isChecked) {
+                setState(() {
+                  _vegetarianFilterSet = isChecked;
+                });
+              },
+            ),
+            SwitchListTile(
+              contentPadding: const EdgeInsets.only(
+                left: 34,
+                right: 22,
+              ),
+              activeColor: Theme.of(context).colorScheme.tertiary,
+              subtitle: Text(
+                'Only include vegan meals.',
+                style: Theme.of(context)
+                    .textTheme
+                    .labelMedium!
+                    .copyWith(color: Theme.of(context).colorScheme.onSurface),
+              ),
+              title: Text(
+                'Vegan',
+                style: Theme.of(context)
+                    .textTheme
+                    .titleLarge!
+                    .copyWith(color: Theme.of(context).colorScheme.onSurface),
+              ),
+              value: _veganFilterSet,
+              onChanged: (isChecked) {
+                setState(() {
+                  _veganFilterSet = isChecked;
+                });
+              },
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
